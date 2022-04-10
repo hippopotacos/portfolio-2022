5443ab8ab7a2084beeb3b658dcf10dce7063661b
hippopotacos <hippopotacos@gmail.com>
2022-04-10 15:55:20 +0900

    [削除]microCMSを利用しないよう修正

--- my-portfolio/libs/client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-  serviceDomain: 'portfolio-hippopotacos',
-  apiKey: process.env.API_KEY,
-});
--- my-portfolio/pages/index.js
+++ my-portfolio/pages/index.js
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import { client } from "../libs/client";
 import styles from "../styles/Home.module.css"
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
@@ -10,27 +8,10 @@ export default function Home({ works }) {
       <Header />
       <main className={styles.main}>
         <ul>
-          {works.map((works) => (
-            <li key={works.id}>
-              <Link href={`/works/${works.id}`}>
-                <a>{works.title}</a>
-              </Link>
-            </li>
-          ))}
+          <li>test</li>
         </ul>
       </main>
       <Footer />
     </>
   );
 }
-
-// データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "works" });
-
-  return {
-    props: {
-      works: data.contents
-    }
-  };
-};
