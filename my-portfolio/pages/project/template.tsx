a9305d760d7ad92471d2e49e8b929d85f324188d
hippopotacos <hippopotacos@gmail.com>
2022-06-03 23:22:40 +0900

    [追加]project概要ページ作成

---
+++ my-portfolio/pages/project/template.tsx
@@ -0,0 +1,51 @@
+import Header from "../../components/Header"
+import Main from "../../components/Main"
+import Footer from "../../components/Footer"
+import Button from "../../components/Button"
+import Content from "../../components/Content"
+
+export default function Template() {
+  return (
+    <>
+      <Header />
+      <Main>
+        <Content>
+          <h1>h1です</h1>
+          <p>概要です。ここに<strong>strong</strong>。ここに<b>bold</b>。ここに<a href="/">link</a>。</p>
+          <section>
+            <h2>h2です</h2>
+            <p>こんにちは</p>
+            <section>
+              <h3>h3です</h3>
+              <p>以下のとおり</p>
+              <ul>
+                <li>a</li>
+                <li>b</li>
+              </ul>
+            </section>
+            <section>
+              <h3>h3です</h3>
+              <ol>
+                <li>aa</li>
+                <li>bb</li>
+              </ol>
+            </section>
+          </section>
+          <section>
+            <h2>h2</h2>
+            <p>テスト</p>
+            <figure>
+              <img src="https://placehold.jp/1250x50.png" alt="test" />
+              <figcaption>test</figcaption>
+            </figure>
+            <p>あはは</p>
+          </section>
+        </Content>
+        <Button href="/">
+          TOPページにもどる
+        </Button>
+      </Main>
+      <Footer />
+    </>
+  )
+}
