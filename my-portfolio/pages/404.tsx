6fe687ffdabd81b2c016551d7f78829d553fa6f8
hippopotacos <hippopotacos@gmail.com>
2022-06-07 18:17:21 +0900

    [修正]404ページの修正

--- my-portfolio/pages/404.tsx
+++ my-portfolio/pages/404.tsx
@@ -1,7 +1,28 @@
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Content from "../components/Content";
+import Image from "next/image";
+import Button from "../components/Button";
+import Footer from "../components/Footer";
+
 export default function Custom404() {
   return (
-    <main className="main">
-      <p>ページがありません。</p>
-    </main>
+    <>
+      <Header />
+      <Main>
+        <Content>
+          <div>
+            <h1>
+              <Image src="/images/404.svg" alt="404 not found" width={135} height={78} />
+            </h1>
+            <p>お探しのページが見つかりません。<br />入力したアドレスが間違っているか、移動もしくは削除された可能性があります。</p>
+          </div>
+        </Content>
+        <Button href="/">
+          TOPページにもどる
+        </Button>
+      </Main>
+      <Footer playlist />
+    </>
   );
 }
