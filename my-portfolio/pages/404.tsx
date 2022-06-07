02919f18efc76f8add70c2a5c759260cd87c90f9
hippopotacos <hippopotacos@gmail.com>
2022-06-07 18:21:54 +0900

    [修正]404ページにheadのデータ追加、404ページにおける不要なdivの削除

--- my-portfolio/pages/404.tsx
+++ my-portfolio/pages/404.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Content from "../components/Content";
@@ -8,15 +9,17 @@ import Footer from "../components/Footer";
 export default function Custom404() {
   return (
     <>
+      <Head>
+        <title>お探しのページが見つかりません - Kazunori MATSUNAGA Portfolio</title>
+        <link rel="shortcut icon" href="/images/favicon.svg" />
+      </Head>
       <Header />
       <Main>
         <Content>
-          <div>
-            <h1>
-              <Image src="/images/404.svg" alt="404 not found" width={135} height={78} />
-            </h1>
-            <p>お探しのページが見つかりません。<br />入力したアドレスが間違っているか、移動もしくは削除された可能性があります。</p>
-          </div>
+          <h1>
+            <Image src="/images/404.svg" alt="404 not found" width={135} height={78} />
+          </h1>
+          <p>お探しのページが見つかりません。<br />入力したアドレスが間違っているか、移動もしくは削除された可能性があります。</p>
         </Content>
         <Button href="/">
           TOPページにもどる
