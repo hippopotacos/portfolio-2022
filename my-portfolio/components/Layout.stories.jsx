e951dba547b1b206a29ed3c7f5e89be63b9abf38
hippopotacos <hippopotacos@gmail.com>
2022-06-24 22:28:01 +0900

    [修正]storiesファイルの軽微な修正

--- my-portfolio/components/Layout.stories.jsx
+++ my-portfolio/components/Layout.stories.jsx
@@ -5,21 +5,9 @@ export default {
   component: Layout
 }
 
-export const Primary = () => {
+export const Default = () => {
   return (
     <Layout>
-      <h1>h1</h1>
-      <p>paragraph</p>
-      <h2>h2</h2>
-      <p>paragraph</p>
-      <h3>h3</h3>
-      <p>paragraph</p>
-    </Layout>
-  )
-}
-export const Secondary = () => {
-  return (
-    <Layout works>
       <h1>h1</h1>
       <p>paragraph</p>
       <h2>h2</h2>
